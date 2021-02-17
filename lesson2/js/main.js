3db3735e961cbce575b028e887873d1ac96773b8
DmitryMarkovi <78977528+DmitryMarkovi@users.noreply.github.com>
2021-02-17 16:23:03 +0500

    good 4 Be more attentive

---
+++ lesson2/js/main.js
@@ -0,0 +1,78 @@
+
+
+
+// Переменные примитивные 
+
+myVar = 10;
+
+myVar = 'Hello world';
+
+myVar = true;
+
+myVar = null;
+
+myVar = undefined;
+
+myVar = Symbol();
+
+
+
+
+myVar = {};
+console.log( typeof myArr);
+
+
+// Объектные переменные 
+
+myArr = [];
+
+let RegExp = /w+/g;
+
+let func = function(){};
+
+let error = Error('error massage');
+
+let date = new Date();
+
+
+// Операторы 1) || <-- или    2) && <--  И  3) ! <-- оператор НЕ
+
+
+// Числа
+
+console.log(50); // Целое числа
+console.log(8.5); // Дробное
+console.log(.5); // Если целое дробного числа ровна 0 можно не указывать число 
+console.log(5e6); // Экзистенциальное число
+console.log(0b1111); // Двоичное 
+console.log(0X2222); // Восьмеричное
+
+
+
+let money = 6000800;
+
+console.log(money);
+console.log(typeof money);
+
+let income = 'Events';
+
+console.log(typeof income);
+
+let addExpenses = 'Регине на подарки, На дом, на Инвестиции';
+
+console.log(addExpenses.length);
+console.log(  addExpenses.toLowerCase());
+console.log(  addExpenses.split(', '));
+
+let deposit = true;
+
+let mission  = 8934900000;
+
+let period = 12;
+
+console.log( period = mission);
+
+
+let budgetDay = '1000000р доход за месяц';
+
+console.log(budgetDay);
